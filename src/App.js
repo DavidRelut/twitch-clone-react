7f05032a753cabf64c5382eb5d6fa6ac935584b3
David R <codingaccelerator@gmail.com>
2022-08-05 14:01:27 +0200

    Refactor Header component, use matchmedia to create header display logic for large and small devices and icon changes when modified + update media queries from CSS + manage result of search with Result & Erreur component + style Result component

--- src/App.js
+++ src/App.js
@@ -4,23 +4,31 @@ import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Games from './components/Games/Games'
 import TopStreams from './components/TopStreams/TopStreams';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Router } from 'react-router-dom';
 import Live from './components/Live/Live';
 import GameStreams from './components/GameStreams/GameStreams';
+import Resultats from './components/Resultats/Resultats';
+import Erreur from './components/Erreur/Erreur';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Sidebar />
+    <Router
+    forceRefresh={true}
+    >
+      <div className="App">
+        <Header />
+        <Sidebar />
 
-      <Routes>
-        <Route path='/' element={<Games/>} />
-        <Route path='/top-streams' element={<TopStreams/>} />
-        <Route path='/live/:slug' element={<Live/>} />
-        <Route path='/game/:slug' element={<GameStreams/>} />
-      </Routes>
-    </div>
+        <Routes>
+          <Route path="/" element={<Games />} />
+          <Route path="/top-streams" element={<TopStreams />} />
+          <Route path="/live/:slug" element={<Live />} />
+          <Route path="/game/:slug" element={<GameStreams />} />
+          <Route path="/resultats/:slug" element={<Resultats />} />
+          <Route path="/resultats/" element={<Erreur />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
