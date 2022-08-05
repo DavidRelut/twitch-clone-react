83a1c15fc7457cfa5c0969c4ff9619280aa1410f
David R <codingaccelerator@gmail.com>
2022-08-05 14:03:31 +0200

    create logical when streamer is offline

--- src/components/Live/Live.js
+++ src/components/Live/Live.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react'
-import ReactTwitchEmbedVideo from 'react-twitch-embed-video'
-import { useParams } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import ReactTwitchEmbedVideo from 'react-twitch-embed-video';
+import {useParams} from 'react-router-dom';
 import api from '../../api';
 
-function Live() {
+function Live(){
 
-  let {slug} = useParams();
-  console.log(slug);
-
-  const [infoStream, setInfoStream] = useState([]);
-  const [infoGame, setInfoGame] = useState([]);
+    let {slug} = useParams();
+    console.log(slug);
+    
+    const [infoStream, setInfoStream] = useState([]);
+    const [infoGame, setInfoGame] = useState([]);
 
     useEffect(() => {
 
         const fetchData = async () => {
 
             const result = await api.get(`https://api.twitch.tv/helix/streams?user_login=${slug}`)
-            // console.log(result);}`)
+            // console.log(result);
             
             if(result.data.data.length === 0) {
                 setInfoStream(false)
@@ -45,21 +45,30 @@ function Live() {
     }, [slug])
 
 
+    return (
 
+        infoStream  ?
 
+        <div className="containerDecale">
+            <ReactTwitchEmbedVideo height="754" width="100%" channel={slug}/>
+            <div className="contInfo">
+                <div className="titreStream">{infoStream.title}</div>
+                <div className="viewer">Viewers : {infoStream.viewer_count}</div>
+                <div className="infogame">Streamer : {infoStream.user_name}, &nbsp; Langue : {infoStream.language}</div>
+                <div className="nomJeu">Jeu : {infoGame}</div>
+            </div>
+        </div>
 
+        :
 
-  return (
-    <div className="containerDecale">
-      <ReactTwitchEmbedVideo height="754" width="100%" channel={slug} />
-      <div className="contInfo">
-        <div className="titreStream">{infoStream.title}</div>
-        <div className="viewer">Viewers : {infoStream.viewer_count}</div>
-        <div className="infogame">Streamer : {infoStream.user_name}, &nbsp; Langue :{' '} {infoStream.language}</div>
-        <div className="nomJeu">Jeu : {infoGame}</div>
-      </div>
-    </div>
-  );
+        <div className="containerDecale">
+            <ReactTwitchEmbedVideo height="754" width="100%" channel={slug}/>
+            <div className="contInfo">
+                <div className="titreStream">Le Streamer est offline ! </div>
+            </div>
+        </div>
+
+    )
 }
 
-export default Live
+export default Live;
